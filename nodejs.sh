e1c8d963fad7244bf6cedd553268f0c35ab8ea93
AdRiley <riley.ad@gmail.com>
2014-01-13 11:44:25 +0000

    added nodejs.sh

---
+++ nodejs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NODE=/opt/node/bin/node
+SERVER_JS_FILE=/home/pi/nodejs/http_server.js
+USER=pi
+OUT=/home/pi/nodejs/nodejs.log
+
+case "$1" in
+
+start)
+	echo "starting node: $NODE $SERVER_JS_FILE"
+	sudo -u $USER $NODE $SERVER_JS_FILE > $OUT 2>$OUT &
+	;;
+
+stop)
+	killall $NODE
+	;;
+
+*)
+	echo "usage: $0 (start|stop)"
+esac
+
+exit 0
+
